f827209eb84ba18bcfce9fd0037f56c38b7bff44
Hardik Gayner <hardikgayner@gmail.com>
2026-03-05 19:34:57 +0530

    Added: Protected Routes

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -4,11 +4,11 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     return (
         <AuthContext.Provider value={{user, setUser, loading, setLoading}}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
